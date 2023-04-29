162996ef088de13dacdf1ce91ea76491e9fc00f7
Lucas Oliveira <lucas.ruas@gmail.com>
2023-04-29 10:51:44 -0300

    Update LogLevel.java

--- src/main/java/org/example/LogLevel.java
+++ src/main/java/org/example/LogLevel.java
@@ -1,6 +1,6 @@
 package org.example;
 
-/*Singletons are almost like a normal class. Therefore, they can also have methods and attributes.
+/*Singletons are almost like normal classes. Therefore, they can also have methods and attributes.
 * They also implement Comparable by default and the compareTo method behaviour is that the first
 * value is the lowest and the last is the highest.
 */
